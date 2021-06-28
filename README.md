3986f6482554ce59b46135b68f3fd59486607a8c
Thang <tan@demo.co.jp>
2021-06-28 18:53:41 +0900

    updated

--- README.md
+++ README.md
@@ -22,3 +22,10 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### ###################################################
+
+### Tạo 1 số game, hoặc app (list product)
+
+### ###################################################
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <navigation/>
+    <router-view></router-view>
+    <Footer/>
+
   </div>
 </template>
+
+  
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Navigation from './components/includes/nav.vue'
+import Footer from './components/includes/footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation,
+    Footer
   }
 }
 </script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
